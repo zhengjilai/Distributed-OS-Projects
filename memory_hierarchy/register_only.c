57797d2aecd682da5a5a0c3029d9d6a49cbe748c
zhengjilai <zhengjilai5816@163.com>
2019-11-07 15:18:44 +0800

    fix register only

--- memory_hierarchy/register_only.c
+++ memory_hierarchy/register_only.c
@@ -6,31 +6,29 @@
  */
 
 int main() { 
-    int sum = 0;
     int maxnum = MAXNUM;
+    int res = -1;
     // the assemble language code in C
     __asm__ __volatile__(
-        "_start: \n"
-        "pushq %%rax \n\t"
+        "_start: pushq %%rax \n\t"
         "pushq %%rbx \n\t"
         "pushq %%rcx \n\t"
-        "movl %2, %%eax \n\t"
-        "movl %1, %%ebx \n\t"
+        "xor %%eax, %%eax \n\t"
+        "mov %1, %%ebx \n\t"
         "xor %%ecx, %%ecx\n\t"
-        "_loop: \n"
-        "add 4, %%ecx \n\t"
+        "_loop: add $1, %%ecx \n\t"
         "inc %%eax \n\t"
         "inc %%eax \n\t"
         "inc %%eax \n\t"
         "inc %%eax \n\t"
-        "cmpl %%eax, %%ebx \n\t"
+        "cmpl %%ecx, %%ebx \n\t"
         "jl _loop \n\t"
-        "movl %%eax, %0 \n\t"
+        "mov %%eax, %0 \n\t"
         "popq %%rcx \n\t"
         "popq %%rbx \n\t"
         "popq %%rax \n\t"
-        : "=r" (sum)
-        : "r" (maxnum), "r" (sum)
+        : "=r" (res)
+        : "r" (maxnum): "memory"
     );
-    printf("Result for register only accumulation is %d\n", sum);
+    printf("Result for register only accumulation is %d\n", res);
 } 
